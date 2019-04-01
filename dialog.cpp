1e19ea9e93bd9a42bc7b98cd9291f17fa7f4d53e
sdelsm00 <36730316+delSerSergio@users.noreply.github.com>
2019-04-02 01:40:31 +0200

    Faltan los retoque finales y demás material.

--- dialog.cpp
+++ dialog.cpp
@@ -1,6 +1,9 @@
 #include <QFileDialog>
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <iostream>
+
+using namespace std;
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -16,29 +19,58 @@ Dialog::~Dialog()
 
 void Dialog::on_pushButton_clicked()
 {
-    ruta=QFileDialog::getExistingDirectory();
-    ui->lineEdit->setText(ruta);
-    if(ui->lineEdit->text() !="")
+    clock_t t_inicio, t_fin;
+    double secs, milisecs;
+
+    int tam=5, i, j, NUM_MAX=10000, p, k, aux;
+    double tiempos[tam];
+    double media=0;
+    long arrayDes[NUM_MAX];
+
+
+    for(i=0;i<tam;i++)
     {
-            ui->pushButton->setEnabled(true);
-            ui->pushButton_2->setEnabled(true);
+        t_inicio=clock();
+        FILE* documento=fopen("Salida.txt", "rt");
+        for(j=0;j<NUM_MAX;j++)
+        {
+            long num;
+            fscanf(documento, "%ld", &num);
+            arrayDes[j]=num;
+        }
+
+        fclose(documento);
+
+        for(p=0;p<NUM_MAX;p++)
+        {
+            for (k=p+1;k<NUM_MAX;k++)
+            {
+                if(arrayDes[p]>arrayDes[k])
+                {
+                    aux=arrayDes[p];
+                    arrayDes[p]=arrayDes[k];
+                    arrayDes[k]=aux;
+                }
+            }
+        }
+
+        FILE* numerosOrdenados=fopen("numOrd.txt", "w");
+        for(j=0;j<NUM_MAX;j++)
+        {
+            fprintf(numerosOrdenados, "%ld ", arrayDes[j]);
+        }
+
+        fclose(numerosOrdenados);
+
+        t_fin=clock();
+        tiempos[i]=(double)(t_fin-t_inicio)/CLOCKS_PER_SEC;
+        media=media+tiempos[i];
     }
-}
 
-void Dialog::on_pushButton_2_clicked()
-{
-    rutaDestino=QFileDialog::getExistingDirectory();
-    ui->lineEdit_2->setText(rutaDestino);
-    if(ui->lineEdit_2->text() !="")
-    {
-        ui->pushButton_3->setEnabled(true);
-    }
-}
-
-void Dialog::on_pushButton_3_clicked()
-{
-    clock_t t_inicio, t_final;
-    double segundos, milisegundos;
-
-    t_inicio=clock();
+    ui->lineEdit_3->setText(QString::number(tiempos[0])+" segundos.");
+    ui->lineEdit_4->setText(QString::number(tiempos[0])+" segundos.");
+    ui->lineEdit_5->setText(QString::number(tiempos[0])+" segundos.");
+    ui->lineEdit_6->setText(QString::number(tiempos[0])+" segundos.");
+    ui->lineEdit_7->setText(QString::number(tiempos[0])+" segundos.");
+    ui->lineEdit_8->setText(QString::number(media)+" segundos.");
 }
--- dialog.h
+++ dialog.h
@@ -16,16 +16,11 @@ public:
     ~Dialog();
 
 private slots:
+
     void on_pushButton_clicked();
-
-    void on_pushButton_2_clicked();
-
-    void on_pushButton_3_clicked();
 
 private:
     Ui::Dialog *ui;
-    QString ruta;
-    QString rutaDestino;
 };
 
 #endif // DIALOG_H
